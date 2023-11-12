39d583f6c933a249df3e6960dc49cd8e735c44e1
alexandreservian <alexandreservian@gmail.com>
2023-11-12 12:53:03 -0300

    refactor: animacao no pivot

--- Assets/Scripts/MiniGames/BreakBar.cs
+++ Assets/Scripts/MiniGames/BreakBar.cs
@@ -1,10 +1,13 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class BreakBar : MonoBehaviour
 {
+    [Header("Pivot Settings:")]
     [SerializeField] private GameObject pivot;
+    [SerializeField] [Range(0.5f, 1.5f)] private float duration = 6f;
     void Start()
     {
-        
+        pivot.transform.DOMoveY(-0.75f, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 }
