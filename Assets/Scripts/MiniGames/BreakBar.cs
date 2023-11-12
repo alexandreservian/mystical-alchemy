2eb1fed6b1559a11bda68eb8fceb8898d7c4a82b
alexandreservian <alexandreservian@gmail.com>
2023-11-12 19:54:09 -0300

    refactor: zerar novamente o tamanho do slidebar caso o user erre

--- Assets/Scripts/MiniGames/BreakBar.cs
+++ Assets/Scripts/MiniGames/BreakBar.cs
@@ -12,26 +12,33 @@ public class BreakBar : MonoBehaviour
     [Header("Mini Game Settings:")]
     [SerializeField] [Range(3, 6)] private int numberOfHits = 3;
     [Header("Pivot Settings:")]
-    [SerializeField] [Range(0.5f, 1.5f)] private float duration = 6f;
+    [SerializeField] [Range(0.5f, 1.5f)] private float durationPivot = 0.5f;
+    [Header("Slider Bar Settings:")]
+    [SerializeField] [Range(0.2f, 1f)] private float durationSliderBar = 1f;
     private float limitIndicateBreakTop;
     private float limitIndicateBreakBottom;
     private float multipleScaleSliderBar;
     void Start()
     {
+        var multipleScalar = (bar.bounds.size.y / sliderBarSprite.bounds.size.y) / numberOfHits;
         limitIndicateBreakTop = indicateBreak.bounds.max.y;
         limitIndicateBreakBottom = indicateBreak.bounds.min.y;
-        multipleScaleSliderBar = (bar.bounds.size.y / sliderBarSprite.bounds.size.y) / numberOfHits;
-        //sliderBar.transform.localScale = new Vector3(1,0,0);
+        multipleScaleSliderBar = Mathf.Floor(multipleScalar * 100.0f) / 100.0f;
+        sliderBar.transform.localScale = new Vector3(1,0,0);
         var endPosition = bar.bounds.min.y + 0.11f;
-        pivot.transform.DOMoveY(endPosition, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        pivot.transform.DOMoveY(endPosition, durationPivot).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 
     private void Update() {
         var positionPivotY = pivot.transform.position.y;
         var isInsideLimit = positionPivotY <= limitIndicateBreakTop && positionPivotY >= limitIndicateBreakBottom;
-        if (InputManager.instance.isPressedTouch && isInsideLimit) {
-            var newScale = sliderBar.transform.localScale.y + multipleScaleSliderBar;
-            sliderBar.transform.DOScaleY(newScale, 1);
+        if (InputManager.instance.isPressedTouch) {
+            if(isInsideLimit) {
+                var newScale = sliderBar.transform.localScale.y + multipleScaleSliderBar;
+                sliderBar.transform.DOScaleY(newScale, durationSliderBar);
+            }else{
+                sliderBar.transform.DOScaleY(0, durationSliderBar);
+            }
         }
     }
 }
