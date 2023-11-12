7c64fd7fe7175917bd5617e34351b056837e4ec3
alexandreservian <alexandreservian@gmail.com>
2023-11-12 14:00:55 -0300

    refactor: definir limite automatico onde o pivo pode ir

--- Assets/Scripts/MiniGames/BreakBar.cs
+++ Assets/Scripts/MiniGames/BreakBar.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class BreakBar : MonoBehaviour
 {
-    [Header("Pivot Settings:")]
+    [Header("Game Objects:")]
+    [SerializeField] private SpriteRenderer bar;
+    [SerializeField] private GameObject indicateBreak;
     [SerializeField] private GameObject pivot;
+    [Header("Pivot Settings:")]
     [SerializeField] [Range(0.5f, 1.5f)] private float duration = 6f;
     void Start()
     {
-        pivot.transform.DOMoveY(-0.75f, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        var endPosition = bar.bounds.min.y + 0.11f;
+        pivot.transform.DOMoveY(endPosition, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 }
