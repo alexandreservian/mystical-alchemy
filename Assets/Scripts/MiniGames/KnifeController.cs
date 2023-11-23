1494c92ce0010a44e048019d560f001e1d62f62f
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-11-23 19:50:48 -0300

    add knife sound; cut fixes

--- Assets/Scripts/MiniGames/KnifeController.cs
+++ Assets/Scripts/MiniGames/KnifeController.cs
@@ -87,6 +87,7 @@ public class KnifeController : MonoBehaviour, IPointerDownHandler, IBeginDragHan
             {
                 successCount = 0;
 
+                SoundManager.Instance.PlaySfx("Knife Cut");
                 animator.enabled = true;
                 OnCutSucceeded?.Invoke();
                 enabled = false;
