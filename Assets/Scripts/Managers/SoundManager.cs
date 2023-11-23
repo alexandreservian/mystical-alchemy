05d13c9b0fa82d7b67894f3a15cd16a1ce79f637
alexandreservian <alexandreservian@gmail.com>
2023-11-23 08:08:33 -0300

    feat: criado sound Manager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+    public string name;
+    public AudioClip audioClip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    [SerializeField] List<Sound> sounds;
+    [SerializeField] AudioSource musicSounds, sfxSounds;
+    void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(string soundName) {
+        Sound sound = sounds.Find(s => s.name == soundName);
+
+        if(sound != null) {
+            musicSounds.clip = sound.audioClip;
+            musicSounds.Play();
+        }
+    }
+
+    public void PlaySfx(string soundName) {
+        Sound sound = sounds.Find(s => s.name == soundName);
+
+        if(sound != null) {
+            sfxSounds.PlayOneShot(sound.audioClip);
+        }
+    }
+}
