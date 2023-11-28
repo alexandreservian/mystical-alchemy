89bfb3b2f2992b425017e3813ec7233ffdeb7af7
alexandreservian <alexandreservian@gmail.com>
2023-11-28 20:10:51 -0300

    refactor: adicionando pause ao game ao abrir a receita, e adicionando spam para o break egg

--- Assets/Scripts/Huds/HudRecipe.cs
+++ Assets/Scripts/Huds/HudRecipe.cs
@@ -4,12 +4,18 @@ public class HudRecipe : MonoBehaviour
 {
     [SerializeField] RectTransform panel;
 
+    private void Start() {
+        OnShowRecipePanel();
+    }
+
     public void OnShowRecipePanel() {
+        Time.timeScale = 0;
         panel.gameObject.SetActive(true);
         InputManager.PlayerInput.SwitchCurrentActionMap("Menu");
     }
 
     public void OnCloseRecipePanel() {
+        Time.timeScale = 1;
         panel.gameObject.SetActive(false);
         InputManager.PlayerInput.SwitchCurrentActionMap("Gameplay");
     }
